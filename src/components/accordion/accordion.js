1f3fb2d7b4f62311d7d77736695e125ea19e0703
unknown <abhishekyesankar12@gmail.com>
2022-12-06 20:14:49 +0530

    replace initialization with class

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,41 +1,97 @@
 "use strict";
 ///you can change prefix if you want to
 const PREFIX = 'jsc', ACCORDIONSELECTOR = `[data-${PREFIX}-accCon]:not([data-${PREFIX}-accCon='false'])`, accordionCon = document.querySelectorAll(ACCORDIONSELECTOR);
-/**
- * TO DO
- * create function or class to create new accordion
- */
+class Accordion {
+    constructor(args) {
+        this.container = '';
+        this.collapsed = true;
+        ///if container argument is empty return
+        if (!args.container)
+            return;
+        this.container = args.container;
+        if (args.button)
+            this.button = args.button;
+        this._init();
+    }
+    _init() {
+        let container = null;
+        ///check if container value is htmlElement or some DOM query
+        if (this.container instanceof HTMLElement) {
+            container = this.container;
+        }
+        else if (typeof this.container === 'string') {
+            container = document.querySelector(this.container);
+        }
+        if (!container)
+            return;
+        ///probably not a good way to do it 
+        this.container = container;
+        ///set new id if the container don't have one
+        if (container.id === '') {
+            const randmoId = Math.floor((Math.random() * 1000) + 1);
+            container.id = `${PREFIX}${randmoId}`;
+        }
+        ///set accordion data
+        if (container.getAttribute(`data-${PREFIX}-accCon`) === null || container.getAttribute(`data-${PREFIX}-accCon`) !== 'false') {
+            container.setAttribute(`data-${PREFIX}-accCon`, 'true');
+        }
+        // @ts-ignore
+        ///only false when collapsed explicitly has false value, default is true
+        this.collapsed = (this.collapsed !== false || this.collapsed !== 'false') ? true : false;
+        container.setAttribute('data-collapse', this.collapsed + '');
+        ///if the collapse value is true hide the element
+        if (this.collapsed)
+            container.style.display = 'none';
+        this._init_target();
+    }
+    _init_target() {
+        if (!this.button)
+            return;
+        let trigger = null;
+        if (this.button instanceof HTMLElement) {
+            trigger = this.button;
+        }
+        else if (typeof this.button === 'string') {
+            trigger = document.querySelector(this.button);
+        }
+        if (trigger) {
+            this._finalizeTarget(trigger);
+            return;
+        }
+        ;
+        if (this.button instanceof HTMLCollection || this.button instanceof NodeList) {
+            const btns = Array.from(this.button);
+            btns.forEach((el) => {
+                const btn = el;
+                if (!btn)
+                    return;
+                this._finalizeTarget(btn);
+            });
+        }
+    }
+    _finalizeTarget(target) {
+        // @ts-ignore
+        const containerId = this.container.id;
+        target.setAttribute(`data-${PREFIX}-target`, containerId);
+        target.setAttribute('aria-expanded', `${!this.collapsed}`);
+        target.setAttribute('aria-controls', containerId);
+        let text;
+        if (this.collapsed) {
+            text = target.getAttribute('data-acccollapsetext');
+            target.classList.add('collapsed');
+        }
+        else {
+            text = target.getAttribute('data-accexpendtext');
+        }
+        if (text !== null)
+            target.innerText = text;
+    }
+}
 accordionCon.forEach(item => {
-    var _a;
-    const id = item.id;
-    ///if initial state not provided of the accordion
-    if (item.dataset.collapse === undefined ||
-        (item.dataset.collapse !== 'false' && item.dataset.collapse !== 'true'))
-        item.setAttribute(`data-collapse`, 'true');
-    if (id !== '')
-        (_a = document.querySelector(`[data-${PREFIX}-target="${id}"]`)) === null || _a === void 0 ? void 0 : _a.setAttribute('aria-controls', id);
-    ///check to see if default accordion collapse data is false or otherwise 
-    ///collapse default data value can be anything
-    ///if it's false then the accordion is expended otherwise collapsed
-    const isCollapse = item.dataset.collapse === 'false' ? false : true;
-    ///initialize container
-    if (isCollapse)
-        item.style.display = 'none';
-    ///adding collapse class to all the triggerer
     const triggerer = document.querySelectorAll(`[data-${PREFIX}-target="${item.id}"]`);
-    triggerer.forEach((el) => {
-        let text = undefined;
-        if (isCollapse) {
-            text = el.dataset.acccollapsetext;
-            el.setAttribute('aria-expanded', 'false');
-            el.classList.add('collapsed');
-        }
-        if (!isCollapse) {
-            text = el.dataset.accexpendtext;
-            el.setAttribute('aria-expanded', 'true');
-            el.classList.remove('collapsed');
-        }
-        text !== undefined && (el.innerText = text);
+    new Accordion({
+        container: item,
+        button: triggerer,
     });
 });
 ///Event Bubbling for Accordion triggerer
@@ -102,74 +158,13 @@ document.body.addEventListener('click', function (e) {
         });
     }
 });
-class Accordion {
-    constructor(args) {
-        this.container = '';
-        this.collapsed = true;
-        ///if container argument is empty return
-        if (!args.container)
-            return;
-        this.container = args.container;
-        // @ts-ignore
-        if (args.collapse !== undefined && (args.collapse === false || args.collapse === 'false'))
-            this.collapsed = false;
-        if (args.button)
-            this.button = args.button;
-        this._init();
-    }
-    _init() {
-        let container = null;
-        ///check if container value is htmlElement or some DOM query
-        if (this.container instanceof HTMLElement) {
-            container = this.container;
-        }
-        else if (typeof this.container === 'string') {
-            container = document.querySelector(this.container);
-        }
-        if (!container)
-            return;
-        ///i know it's weird but doing this because we can use container further
-        this.container = container;
-        ///set new id if the container don't have an ID
-        if (container.id === '') {
-            const randmoId = Math.floor((Math.random() * 1000) + 1);
-            container.id = `${PREFIX}${randmoId}`;
-        }
-        ///set accordion data
-        if (container.getAttribute(`data-${PREFIX}-accCon`) === null || container.getAttribute(`data-${PREFIX}-accCon`) !== 'false') {
-            container.setAttribute(`data-${PREFIX}-accCon`, 'true');
-        }
-        // @ts-ignore
-        ///only false when collapsed explicitly has false value, default is true
-        this.collapsed = (this.collapsed !== false || this.collapsed !== 'false') ? true : false;
-        container.setAttribute('data-collapse', this.collapsed + '');
-        ///if the collapse value is true hide the element
-        if (this.collapsed)
-            container.style.display = 'none';
-        this._init_target();
-    }
-    _init_target() {
-        // @ts-ignore
-        const containerId = this.container.id;
-        let trigger = null;
-        // if( this.button instanceof HTMLElement )  {
-        // }
-        if (!this.button || typeof this.button !== 'string')
-            return;
-        trigger = document.querySelector(this.button);
-        if (!trigger)
-            return;
-        trigger.setAttribute(`data-${PREFIX}-target`, containerId);
-        trigger.setAttribute('aria-expanded', `${!this.collapsed}`);
-        trigger.setAttribute('aria-controls', containerId);
-    }
-}
 const newAccordion = new Accordion({
     container: '#cl-eg-1',
     button: '#cl-eg-1-btn',
 });
+const secondbtn = document.getElementById('cl-eg-2-btn');
 const newAccordion2 = new Accordion({
     container: '.cl-eg-2',
-    button: '#cl-eg-2-btn',
+    button: secondbtn,
 });
 //# sourceMappingURL=accordion.js.map
