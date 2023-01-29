39fff1609f6b754f847cf09d9ff02ca5c2b84a87
unknown <abhishekyesankar12@gmail.com>
2023-01-29 18:53:20 +0530

    remove unnecessary function

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -161,9 +161,6 @@ class JsSlider {
     }
     /** End Controls Functions */
     /** Utilities Functions */
-    _getPointerPosition(e) {
-        return (e instanceof MouseEvent) ? e.clientX : e.touches[0].clientX;
-    }
     _applyResponsiveness() {
         if (this.breakPointsIndex.length > 0) {
             const windowWidth = window.innerWidth;
