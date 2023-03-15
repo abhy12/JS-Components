9e4e04f751aa33df13d48bafdcf267004d34b241
unknown <abhishekyesankar12@gmail.com>
2023-03-15 17:01:19 +0530

    remove terser and ts-loader instead use esbuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
-const path = require('path');
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require( "path" );
+const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
+const { EsbuildPlugin } = require( "esbuild-loader" );
 
 module.exports = {
    entry: {
       accordion: {
          import : './src/components/accordion/accordion.ts',
+         filename: './accordion/accordion.min.js'
       },
       slider: {
          import: './src/components/slider/slider.ts',
-      }
+         filename: './slider/slider.min.js',
+      },
    },
    module: {
       rules: [
          {
-            test: /\.ts?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
+            // Match js, jsx, ts & tsx files
+            test: /\.[jt]sx?$/,
+            loader: 'esbuild-loader',
+            options: {
+               // JavaScript version to compile to
+               target: 'es2015'
+            }
          },
+         {
+            test: /\.css$/i,
+            use: [
+               MiniCssExtractPlugin.loader,
+               "css-loader"
+            ],
+          },
       ],
    },
+   plugins: [
+      new MiniCssExtractPlugin({
+         filename: './[name]/[name].css',
+      }),
+   ],
    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
+      extensions: ['.tsx', '.ts', '.js', '.css'],
    },
    output: {
-      filename: '[name]/[name].min.js',
+      // filename: '[name]/[name].min.js',
       path: path.resolve( __dirname, 'dist' ),
       iife: false,
       clean: true,
@@ -32,15 +51,13 @@ module.exports = {
    optimization: {
       minimize: true,
       minimizer: [
-         new TerserPlugin({
-            terserOptions: {
-               mangle: {
-                  keep_classnames: true,
-               }
-            },
+         new EsbuildPlugin({
+            // target: 'esnext',
+            // minifyIdentifiers: false,
+            // keepNames: true,
+            css: true,
          }),
       ],
    },
-   mode: 'production',
    cache: false,
 }
