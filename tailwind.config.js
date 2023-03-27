9df6589c56ca5566789067d4bf9494063a4cd696
unknown <abhishekyesankar12@gmail.com>
2023-03-27 19:45:42 +0530

    remove js and fix indentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/components/**/*.{html,js}",
-    "./*.html"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: [
+      "./examples/**/*.html",
+   ],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
 }
