f9f28a1ea344749811e4d6fb3434e6c772540e7a
unknown <abhishekyesankar12@gmail.com>
2023-04-19 20:32:26 +0530

    deprecated previous accordion struture

--- src/components/accordion/accordion.ts
+++ src/components/accordion/accordion.ts
@@ -1,45 +1,85 @@
 import { convertHTMLToAccordion } from "./browser";
-import { PREFIX } from "./core";
-import { accordionToggle, accordionToggleEventHandler } from "./trigger";
-import { assignNewIdToElement } from "./utilities";
+import { PREFIX, CONTAINER_ATTR, ACCORDION_ITEM_CONTAINER_SELECTOR, ACCORDION_SELECTOR, TRIGGER_SELECTOR, ACCORDION_ITEM_CONTAINER_ATTR, initAccordion } from "./core";
+import { accordionToggle, accordionToggleEventHandler, getClosestTriggers, getAllAssociateTriggers, Trigger } from "./trigger";
 
-interface AccordionInterace  {
-   container: string | HTMLElement,
-   button: string | Element | HTMLElement | HTMLCollectionOf<HTMLElement> | NodeListOf<HTMLElement> | (HTMLElement| string)[] | undefined | null,
-   collapsed?: boolean | undefined,
+interface AccordionInterface {
+   container: string | HTMLElement | null | undefined,
+   containerIsAccordion?: boolean,
+   accordionItemContainer?: string,
+   accordionEl?: string,
+   button: string | Element | HTMLElement | HTMLCollectionOf<HTMLElement> | NodeListOf<HTMLElement> | (HTMLElement | string)[] | undefined | null,
+   collapsed?: boolean,
    collapseText?: string | undefined,
    expendText?: string | undefined,
    buttonPreventDefault?: boolean
 }
 
-export default class JscAccordion implements AccordionInterace {
+export default class JscAccordion implements AccordionInterface {
    container: HTMLElement;
+   ///for backward compatibility
+   containerIsAccordion: boolean | undefined;
+   accordionItemContainer: string;
+   accordionEl: string | undefined;
    collapsed: boolean;
-   button: AccordionInterace['button'];
-   collapseText: AccordionInterace['collapseText'];
+   button: AccordionInterface['button'];
+   collapseText: AccordionInterface['collapseText'];
    expendText: string;
    buttonPreventDefault: boolean = true;
 
-   constructor( args: AccordionInterace )  {
-      let tempContainer: HTMLElement | string = args.container;
+   constructor( args: AccordionInterface )  {
+      //return if falsy value
+      if( !args.container )  return
 
-      if( typeof tempContainer === 'string' )  {
-         tempContainer = ( document.querySelector( tempContainer ) as HTMLElement );
+      let tempContainer: Element | string | null = args.container;
+
+      if( typeof tempContainer === "string" )  {
+         tempContainer = document.querySelector( tempContainer );
       }
 
-      ///not an element
+      ///don't do anything if container is not an element
       if( !( tempContainer instanceof HTMLElement ) )  return
 
       this.container = tempContainer;
+      this.containerIsAccordion = args.containerIsAccordion;
 
-      ///trigger
-      if( args.button ) this.button = args.button;
+      if( this.containerIsAccordion === false )  {
+         ///needs to be string for searching accordion item container
+         if( typeof args.accordionItemContainer === "string" )  {
+            this.accordionItemContainer = args.accordionItemContainer;
+         } else {
+            ///default
+            this.accordionItemContainer = ACCORDION_ITEM_CONTAINER_SELECTOR;
+         }
 
-      if( args.buttonPreventDefault === false )  this.buttonPreventDefault = false
+         ///accordionEl can only be string if it's something else don't do anything,
+         ///doing this because we want to find the accordion inside the container
+         ///we can't do this by element so it has to be a string
+         if( typeof args.accordionEl === "string" )  {
+            this.accordionEl = args.accordionEl;
+         } else {
+            ///default
+            this.accordionEl = ACCORDION_SELECTOR;
+         }
 
-      const containerCollapseAttributeValue = this.container.getAttribute( 'data-collapse' );
+         ///it has be a string for searching the trigger inside the container
+         if( typeof args.button === "string" )  {
+            this.button = args.button;
+         } else {
+            ///default
+            this.button = TRIGGER_SELECTOR;
+         }
+      } else {
+         ///button can have any other valid values other than only string if
+         ///the containerIsAccordion set to true
+         if( args.button )  this.button = args.button;
+      }
 
-      ///if undefined check if html attribute has the value
+      if( args.buttonPreventDefault === false )  this.buttonPreventDefault = false;
+
+      const containerCollapseAttributeValue = this.container.getAttribute( "data-collapse" );
+
+      ///if arg collapsed is undefined then check if html attribute has the value of "false" and
+      ///if it does then the collapsed will be false.
       ///attribute value needs to be exactly equal to "false" for changing the collapse value
       if( ( args.collapsed === undefined && containerCollapseAttributeValue === "false" ) || args.collapsed === false )  {
          this.collapsed = false;
@@ -52,72 +92,86 @@ export default class JscAccordion implements AccordionInterace {
    }
 
    _init()  {
-      if( !this.container ) return
+      this.container.setAttribute( CONTAINER_ATTR, "true" );
 
-      this.container.style.overflow = 'hidden';
+      ///make container an accordion
+      if( this.containerIsAccordion !== false )  {
+         initAccordion( this.container, this.collapsed );
+         const containerId = this.container.id;
 
-      ///set new id if the container don't have one
-      if( this.container.id === '' )  {
-        assignNewIdToElement( this.container );
-      }
+         if( !this.button )  return
 
-      if( this.container.getAttribute( `data-${PREFIX}-accCon` ) === null || this.container.getAttribute( `data-${PREFIX}-accCon` ) !== 'false' )  {
-         ///set accordion data
-         this.container.setAttribute( `data-${PREFIX}-accCon`, 'true' );
-      }
+         let trigger: null | HTMLElement = null;
 
-      this.container.setAttribute( 'data-collapse', this.collapsed + '' );
+         if( this.button instanceof HTMLElement )  {
+            trigger = this.button;
+         } else if( typeof this.button === "string" )  {
+            trigger = document.querySelector( this.button );
+         }
 
-      ///hide the element
-      if( this.collapsed )  this.container.style.display = 'none';
+         if( trigger )  {
+            this._initTrigger( trigger, containerId );
+            return
+         }
 
-      this._init_trigger();
-   }
+         if( this.button instanceof HTMLCollection || this.button instanceof NodeList || this.button instanceof Array )  {
+            ///@ts-ignore
+            const btns = Array.from( this.button );
 
-   _init_trigger()  {
-      if( !this.button ) return
+            btns.forEach( ( el ) => {
+               if( el instanceof HTMLCollection || el instanceof NodeList )  return
 
-      let trigger: null | HTMLElement = null;
+               let btn: any = el;
 
-      if( this.button instanceof HTMLElement )  {
-         trigger = this.button;
-      } else if( typeof this.button === 'string' )  {
-         trigger = document.querySelector( this.button );
-      }
+               if( typeof btn === 'string' )  {
+                  btn = document.querySelector( btn );
+               }
 
-      if( trigger )  {
-         this._finalizeTrigger( trigger );
-         return
-      }
+               if( !btn )  return
 
-      if( this.button instanceof HTMLCollection || this.button instanceof NodeList || this.button instanceof Array )  {
-         //@ts-ignore
-         const btns = Array.from( this.button );
+               this._initTrigger( btn, containerId );
+            });
+         }
+      } else {
+         ///find all the accordion inside item container
+         const accordionItemsContainer = this.container.querySelectorAll( `${this.accordionItemContainer}` ) as NodeListOf<HTMLElement>;
+         const accordions = this.container.querySelectorAll( `${this.accordionItemContainer} ${this.accordionEl}` ) as NodeListOf<HTMLElement>;
 
-         btns.forEach( ( el ) => {
-            if( el instanceof HTMLCollection || el instanceof NodeList ) return
+         ///add accordion item attribute to every accordion item container
+         accordionItemsContainer.forEach( itemContainer =>  itemContainer.setAttribute( ACCORDION_ITEM_CONTAINER_ATTR, "true" ) );
 
-            let btn: any = el;
+         ///init each accordion and trigger
+         accordions.forEach( ( accordion ) => {
+            initAccordion( accordion );
 
-            if( typeof btn === 'string' )  {
-               btn = document.querySelector( btn );
+            let trigger: Trigger = null;
+
+            if( typeof this.button === "string" )  {
+               ///if set then find all the associate triggers
+               if( accordion.id )  {
+                  trigger = getAllAssociateTriggers( accordion, this.button );
+               }
+               ///if not get all the closest triggers
+               else {
+                  trigger = getClosestTriggers( accordion, this.button );
+               }
             }
 
-            if( !btn ) return
-
-            this._finalizeTrigger( btn );
+            if( trigger )  {
+               trigger.forEach( trigger =>  {
+                  this._initTrigger( trigger, accordion.id );
+               });
+            }
          });
       }
    }
 
-   _finalizeTrigger( target: HTMLElement )  {
-      const containerId = this.container.id;
+   _initTrigger( trigger: HTMLElement, targetId: string )  {
+      trigger.setAttribute( `data-${PREFIX}-target`, targetId );
+      trigger.setAttribute( 'aria-expanded', `${!this.collapsed}` );
+      trigger.setAttribute( 'aria-controls', targetId );
 
-      target.setAttribute( `data-${PREFIX}-target`, containerId );
-      target.setAttribute( 'aria-expanded', `${!this.collapsed}` );
-      target.setAttribute( 'aria-controls', containerId );
-
-      if( this.buttonPreventDefault === false )  target.setAttribute( `data-${PREFIX}-preventdefault`, 'false' );
+      if( this.buttonPreventDefault === false )  trigger.setAttribute( `data-${PREFIX}-preventdefault`, 'false' );
 
       let collapseOrExpendText: string | null;
 
@@ -126,20 +180,20 @@ export default class JscAccordion implements AccordionInterace {
          if( this.collapseText !== undefined )  {
             collapseOrExpendText = this.collapseText;
          } else {
-            collapseOrExpendText = target.getAttribute( 'data-collapsetext' );
+            collapseOrExpendText = trigger.getAttribute( 'data-collapsetext' );
          }
 
-         target.classList.add( 'collapsed' );
-      } else {
+         trigger.classList.add( 'collapsed' );
 
+      } else {
          if( this.expendText !== undefined )  {
             collapseOrExpendText = this.expendText;
          } else {
-            collapseOrExpendText = target.getAttribute( 'data-expendtext' );
+            collapseOrExpendText = trigger.getAttribute( 'data-expendtext' );
          }
       }
 
-      if( collapseOrExpendText !== null ) target.innerText = collapseOrExpendText;
+      if( collapseOrExpendText !== null )  trigger.innerText = collapseOrExpendText;
    }
 
    enable()  {
