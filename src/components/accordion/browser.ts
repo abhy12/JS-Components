4583ebec751aae8ba44f39a1f8fa6d6aa7370403
unknown <abhishekyesankar12@gmail.com>
2023-04-19 20:35:16 +0530

    deprecated previous accordion struture

--- src/components/accordion/browser.ts
+++ src/components/accordion/browser.ts
@@ -1,30 +1,64 @@
-import { ACCORDIONSELECTOR, PREFIX } from "./core";
+import { ACCORDION_SELECTOR, CONTAINER_SELECTOR, CONTAINER_ATTR, DEP_ACCORDION_SELECTOR } from "./core";
 import JscAccordion from "./accordion";
+import { getClosestTriggers, getAllAssociateTriggers } from "./trigger";
+
+function addNewAttributeToDeprecatedAccordion()  {
+   const deprecatedAccordion = document.querySelectorAll( DEP_ACCORDION_SELECTOR );
+
+   deprecatedAccordion.forEach( accordion => {
+      ///get previous attribute value then assign to new attribute value
+      accordion.setAttribute( CONTAINER_ATTR, 'true' );
+   });
+   ///P.S not removing the deprecated attribute because if user
+   ///have set the style based on the attribute
+}
 
 export function convertHTMLToAccordion()  {
-   ///get all the accordion content container
-   const accordions = document.querySelectorAll( ACCORDIONSELECTOR ) as NodeListOf<HTMLElement>;
+   ///for backward compatibility
+   addNewAttributeToDeprecatedAccordion();
 
-   ///convert accordion container to "accordion"
-   accordions.forEach( accordion =>  {
-      let trigger;
-      const accId = accordion.id;
+   const accordionContainers = document.querySelectorAll( CONTAINER_SELECTOR ) as NodeListOf<HTMLElement>;
 
-      if( accId === '' )  {
-         ///not selecting all the trigger elements
-         ///because of nested accordion under the container
-         trigger = accordion.closest( '.jsc-accordion' )?.querySelector( `[data-${PREFIX}-target]` ) as HTMLElement;
-      } else if( accId !== '' )  {
-         trigger = document.querySelectorAll( `[data-${PREFIX}-target="${accordion.id}"]` ) as NodeListOf<HTMLElement>;
+   accordionContainers.forEach( container => {
+      let trigger: null | NodeListOf<HTMLElement> | HTMLElement[] = null;
+
+      ///container accordions
+      const accordions = container.querySelectorAll( ACCORDION_SELECTOR ) as NodeListOf<HTMLElement>;
+
+      if( accordions.length > 0 )  {
+         accordions.forEach( accordion => {
+            const accordionId = accordion.id;
+
+            ///accordion don't have ID that means it don't have any
+            ///trigger outside the item container
+            if( accordionId === '' )  {
+               trigger = getClosestTriggers( accordion );
+            } else if( accordionId !== '' )  {
+               trigger = getAllAssociateTriggers( accordion );
+            }
+
+            new JscAccordion({
+               container: accordion,
+               button: trigger,
+            });
+         });
       }
 
-      ///definitely not a good idea to use this class in this file
-      ///and after that this file will be used in main "Accordion" file
-      ///i don't have any idea right now how can i solve this.
-      ///doing this because separation
-      new JscAccordion({
-         container: accordion,
-         button: trigger,
-      });
+      ///make container an accordion,
+      ///doing this because of backward compatibility
+      else if( accordions.length === 0 )  {
+         const containerId = container.id;
+
+         if( containerId === '' )  {
+            trigger = getClosestTriggers( container );
+         } else if( containerId !== '' )  {
+            trigger = getAllAssociateTriggers( container );
+         }
+
+         new JscAccordion({
+            container: container,
+            button: trigger,
+         });
+      }
    });
 }
