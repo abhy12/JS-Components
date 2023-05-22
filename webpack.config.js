a24a86465d99f0beb46a88c1e4d45c304b6b55f3
unknown <abhishekyesankar12@gmail.com>
2023-05-22 18:33:00 +0530

    chore: fix scripts not working on windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,70 @@
-const path = require( "path" );
-const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
-const { EsbuildPlugin } = require( "esbuild-loader" );
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { EsbuildPlugin } = require("esbuild-loader");
 
 ///ENVIRONMENT
-const environment = process.env.NODE_ENV.trim();
-const envOutputDir = environment === 'development' ? 'examples' : 'src/components';
-const isMin = environment === 'development' ? '' : '.min';
+module.exports = ( _, argv ) => {
+   const environment = argv.mode;
+   const envOutputDir = environment === 'development' ? 'examples' : 'src/components';
+   const isMin = environment === 'development' ? '' : '.min';
 
-module.exports = {
-   entry: {
-      accordion: {
-         import : './src/components/accordion/accordion.ts',
-         filename: './accordion/accordion' + isMin +'.js'
-      },
-      slider: {
-         import: './src/components/slider/slider.ts',
-         filename: './slider/slider' + isMin + '.js',
-      },
-   },
-   module: {
-      rules: [
-         {
-            // Match js, jsx, ts & tsx files
-            test: /\.[jt]sx?$/,
-            loader: 'esbuild-loader',
-            options: {
-               // JavaScript version to compile to
-               target: 'es2015'
-            }
+   return  {
+      entry: {
+         accordion: {
+            import: './src/components/accordion/accordion.ts',
+            filename: './accordion/accordion' + isMin + '.js'
          },
-         {
-            test: /\.css$/i,
-            use: [
-               MiniCssExtractPlugin.loader,
-               "css-loader"
-            ],
+         slider: {
+            import: './src/components/slider/slider.ts',
+            filename: './slider/slider' + isMin + '.js',
          },
-      ],
-   },
-   plugins: [
-      new MiniCssExtractPlugin({
-         filename: './[name]/[name]' + isMin + '.css',
-      }),
-   ],
-   resolve: {
-      extensions: ['.tsx', '.ts', '.js', '.css'],
-   },
-   output: {
-      // filename: '[name]/[name].min.js',
-      path: path.resolve( __dirname, envOutputDir ),
-      iife: true,
-      clean: false,
-   },
-   devtool: 'source-map',
-   optimization: {
-      // minimize: true,
-      minimizer: [
-         new EsbuildPlugin({
-            // target: 'esnext',
-            // minifyIdentifiers: false,
-            // keepNames: true,
-            css: true,
+      },
+      module: {
+         rules: [
+            {
+               // Match js, jsx, ts & tsx files
+               test: /\.[jt]sx?$/,
+               loader: 'esbuild-loader',
+               options: {
+                  // JavaScript version to compile to
+                  target: 'es2015'
+               }
+            },
+            {
+               test: /\.css$/i,
+               use: [
+                  MiniCssExtractPlugin.loader,
+                  "css-loader"
+               ],
+            },
+         ],
+      },
+      plugins: [
+         new MiniCssExtractPlugin({
+            filename: './[name]/[name]' + isMin + '.css',
          }),
       ],
-   },
-   cache: false,
+      resolve: {
+         extensions: ['.tsx', '.ts', '.js', '.css'],
+      },
+      output: {
+         // filename: '[name]/[name].min.js',
+         path: path.resolve(__dirname, envOutputDir),
+         iife: true,
+         clean: false,
+      },
+      devtool: 'source-map',
+      optimization: {
+         // minimize: true,
+         minimizer: [
+            new EsbuildPlugin({
+               // target: 'esnext',
+               // minifyIdentifiers: false,
+               // keepNames: true,
+               css: true,
+            }),
+         ],
+      },
+      cache: false,
+   }
 }
