0e2f594f3a3209dd03730e26cc2d262d6ba33294
Abhishek <98876719+abhy12@users.noreply.github.com>
2023-11-21 21:17:06 +0530

    transform assets other then js/ts

--- jest.config.ts
+++ jest.config.ts
@@ -173,7 +173,10 @@ export default {
    // testRunner: "jest-circus/runner",
 
    // A map from regular expressions to paths to transformers
-   // transform: undefined,
+   transform: {
+      "^.+\\.js$": "babel-jest",
+      ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+   }
 
    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
    // transformIgnorePatterns: [
