481f68b065eece9824db2d7451685b649f35b54e
Abhishek <98876719+abhy12@users.noreply.github.com>
2023-12-13 22:19:20 +0530

    change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
    integrations: [tailwind()],
-   // site: "https://abhy12.github.io/JS-Components",
 });
