c87403ab42561ed6c735c7e708e83aff6e376cac
abhy12 <98876719+abhy12@users.noreply.github.com>
2024-02-07 23:49:56 +0530

    test: check expending and collapsing working

--- tests/accordion/trigger.test.ts
+++ tests/accordion/trigger.test.ts
@@ -1,25 +1,37 @@
 import JscAccordion from "@js-components/accordion/";
 import { convertHTMLToAccordion } from "@js-components/accordion/browser";
-import { ACCORDION_SELECTOR, SELECT_TRIGGER_ACCORDION } from "@js-components/accordion/core";
+import { ACCORDION_SELECTOR, SELECT_TRIGGER_ACCORDION, COLLAPSE_ATTR, TRIGGER_SELECTOR, ACCORDION_ITEM_WRAPPER_SELECTOR } from "@js-components/accordion/core";
 
-const accordionBrowserStructure = `
-<div id="basic" data-jsc-accordion-container="">
+jest.useFakeTimers();
+jest.spyOn( global, 'setTimeout' );
+
+const HtmlContainerId = 'basic-html-container',
+accordionBrowserStructure = `
+<div id="${HtmlContainerId}" data-jsc-accordion-container="">
    <div data-jsc-accordion-item="">
-      <h1><button data-jsc-target="">Lorem ipsum dolor sit amet.</button></h1>
+      <h1><button data-jsc-target="">Accordion #1</button></h1>
       <div data-jsc-accordion="">
-         <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, cupiditate sed. Illo itaque eligendi eius.</h2>
+         <h2>Accordion #1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, cupiditate sed. Illo itaque eligendi eius.</h2>
       </div>
    </div>
    <div data-jsc-accordion-item="">
-      <h1><button data-jsc-target="">Lorem ipsum dolor sit amet.</button></h1>
+      <h1><button data-jsc-target="">Accordion #2</button></h1>
       <div data-jsc-accordion="">
-         <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, cupiditate sed. Illo itaque eligendi eius.</h2>
+         <h2>Accordion #2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, cupiditate sed. Illo itaque eligendi eius.</h2>
       </div>
    </div>
+   <div data-jsc-accordion-item="">
+      <h1><button data-jsc-target="">Accordion #3</button></h1>
+      <div id="test-ac-btn" data-jsc-accordion="">
+         <h2>Accordion #3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, cupiditate sed. Illo itaque eligendi eius.</h2>
+      </div>
+   </div>
+   <button data-jsc-target="test-ac-btn">Click to Trigger last accordion</button>
 </div>`;
 
-const customStruture = `
-<div id="eg-1">
+const customStrutureContainerId = "eg-1",
+customStruture = `
+<div id="${customStrutureContainerId}">
    <div class="item">
       <h1><button>Lorem ipsum dolor sit amet.</button></h1>
       <div class="accordion">Lorem ipsum dolor sit amet consect</div>
@@ -28,7 +40,12 @@ const customStruture = `
       <h1><button>Lorem ipsum dolor sit amet.</button></h1>
       <div class="accordion">Lorem ipsum dolor sit amet consect</div>
    </div>
-</div>`;
+   <div class="item">
+      <h1><button>Lorem ipsum dolor sit amet.</button></h1>
+      <div id="test-ac" class="accordion">Lorem ipsum dolor sit amet consect</div>
+   </div>
+</div>
+`;
 
 describe( "accordion trigger", () => {
    beforeEach(() =>  {
@@ -64,6 +81,71 @@ describe( "accordion trigger", () => {
          });
 
          expect( document.querySelectorAll( "#eg-1 .item button" )[1]?.classList.contains( "collapsed" ) ).toBe( true );
+      });
+   });
+
+   describe( "expending and collapsing", () => {
+      test( "html", () => {
+         document.body.insertAdjacentHTML( "afterbegin", accordionBrowserStructure );
+         convertHTMLToAccordion( JscAccordion );
+
+         document.getElementById( HtmlContainerId )?.querySelectorAll( ACCORDION_ITEM_WRAPPER_SELECTOR ).forEach( accordionItem => {
+            if( !( accordionItem instanceof HTMLElement ) ) return
+
+            const trigger = accordionItem.querySelector( TRIGGER_SELECTOR );
+            const accordion = accordionItem.querySelector( ACCORDION_SELECTOR );
+            expect( trigger ).not.toBeFalsy();
+            expect( accordion ).not.toBeFalsy();
+
+            if( trigger instanceof HTMLElement && accordion instanceof HTMLElement ) {
+               const isCollapsed = accordion.dataset['collapse'];
+
+               trigger.click();
+
+               setTimeout(() => {
+                  expect( accordion.dataset['collapse'] ).not.toEqual( isCollapsed );
+               });
+            }
+
+            jest.runAllTimers();
+
+            expect( setTimeout ).toHaveBeenCalled();
+         });
+      });
+
+      test( "class", () => {
+         document.body.insertAdjacentHTML( "afterbegin", customStruture );
+
+         new JscAccordion({
+            container: '#eg-1',
+            accordionElWrapper: '.item',
+            accordionEl: '.accordion',
+            button: '.item button',
+            containerIsAccordion: false
+         });
+
+         document.getElementById( customStrutureContainerId )?.querySelectorAll( ACCORDION_ITEM_WRAPPER_SELECTOR ).forEach( accordionItem => {
+            if( !( accordionItem instanceof HTMLElement ) ) return
+
+            const trigger = accordionItem.querySelector( TRIGGER_SELECTOR );
+            const accordion = accordionItem.querySelector( ACCORDION_SELECTOR );
+            expect( trigger ).not.toBeFalsy();
+            expect( accordion ).not.toBeFalsy();
+
+            if( trigger instanceof HTMLElement && accordion instanceof HTMLElement ) {
+               const isCollapsed = accordion.dataset['collapse'];
+
+               trigger.click();
+
+               setTimeout(() => {
+                  expect( accordion.dataset['collapse'] ).not.toEqual( isCollapsed );
+               });
+            }
+
+            jest.runAllTimers();
+
+            expect( setTimeout ).toHaveBeenCalled();
+         });
       });
    });
 });
