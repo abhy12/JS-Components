4ac68635c0bc1f1e36dcf33f90d83b66ea0a9107
abhy12 <98876719+abhy12@users.noreply.github.com>
2024-06-27 22:59:39 +0530

    add copyright header

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ import path from "path";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 const __dirname = import.meta.dirname;
 import { EsbuildPlugin } from "esbuild-loader";
+import webpack from "webpack";
+import { readFileSync } from "fs";
+const copyrightHeader = readFileSync( "./copyright-header.txt", { encoding: 'utf-8', flag: 'r' } );
 
 export default ( _, argv ) => {
    const environment = argv.mode;
@@ -47,6 +50,24 @@ export default ( _, argv ) => {
       plugins: [
          new MiniCssExtractPlugin({
             filename: './[name]/[name]' + isMin + '.css',
+         }),
+         new webpack.BannerPlugin({
+            banner: ( args ) => {
+               if( environment === 'development' ) return '';
+
+               const baseDir = './src/components/';
+               const entryName = args.chunk.getEntryOptions().name;
+               const entryDir = baseDir + entryName + '/package.json';
+               const { version, name, author, homepage, description } = JSON.parse( readFileSync( entryDir ) );
+
+               let header = copyrightHeader.toString().replaceAll( /{pkg\.name}/g, name );
+               header = header.replaceAll( /{pkg\.version}/g, version );
+               header = header.replaceAll( /{pkg\.author}/g, author );
+               header = header.replaceAll( /{pkg\.description}/g, description );
+               header = header.replaceAll( /{pkg\.url}/g, homepage );
+
+               return header;
+            }
          }),
       ],
       resolve: {
